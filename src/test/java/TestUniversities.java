9182ba3c8e9431ebd944b3a7504302468dfd4395
ahadjioglo <alexandr.hadjioglo@endava.com>
2023-09-28 20:01:18 +0300

    adding logs

--- src/test/java/TestUniversities.java
+++ src/test/java/TestUniversities.java
@@ -1,17 +1,40 @@
 import endpoint.UniversityRestService;
 import models.UniversityResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.assertj.core.api.Assertions;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.stream.Collectors;
 
+import static enums.CountryCodeEnum.RO;
+
 public class TestUniversities {
+    private static final Logger logger = LogManager.getLogger(TestUniversities.class);
 
     @Test
     public void getUniversityOfRomania() {
         UniversityRestService universityRestService = new UniversityRestService();
         var response = universityRestService.getUniversityByCountry("romania").body();
+
+        Assert.assertNotNull(response);
+        var countryCode = response.stream()
+                .map(UniversityResponse::getAlfaTwoCode)
+                .collect(Collectors.toList());
+
+        Assertions.assertThat(countryCode)
+                .contains(RO.name());
+        logger.error("Assertion passed" + countryCode + " contains " + "RO");
+    }
+
+    @Test
+    public void getAllUniversities() {
+        UniversityRestService universityRestService = new UniversityRestService();
+
+        var response = universityRestService.getAllRequest().body();
+        logger.error("response is received");
+
         Assert.assertNotNull(response);
         var countryCode = response.stream()
                 .map(UniversityResponse::getAlfaTwoCode)
@@ -19,5 +42,6 @@ public class TestUniversities {
 
         Assertions.assertThat(countryCode)
                 .contains("RO");
+        logger.error("Assertion passed" + countryCode + " contains " + "RO");
     }
 }
---
+++ src/test/java/enums/CountryCodeEnum.java
@@ -0,0 +1,6 @@
+package enums;
+
+public enum CountryCodeEnum {
+    RO,
+    TR
+}
